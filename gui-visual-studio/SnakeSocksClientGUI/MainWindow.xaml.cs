6c4b2509bce15a4460afda003b6f31c79d063e4e
Ubuntu <ubuntu@ip-172-31-34-139.us-west-2.compute.internal>
2017-08-17 13:49:44 +0000

    anonymous_sync

--- gui-visual-studio/SnakeSocksClientGUI/MainWindow.xaml.cs
+++ gui-visual-studio/SnakeSocksClientGUI/MainWindow.xaml.cs
@@ -14,19 +14,25 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
 using System.Threading;
+using System.Xml;
 
 namespace SnakeSocksClientGUI
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// MainWindow.xaml Logic
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            LoadConfigFile();
         }
-
+        ~ MainWindow()
+        {
+            if (skcli_nogui != null)
+                skcli_nogui.Kill();
+        }
         private void on_filepick_clicked(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
@@ -71,8 +77,10 @@ namespace SnakeSocksClientGUI
             tb_bindaddr.IsEnabled = false;
         }
 
+        private bool isLoadingFrontend = false;
         private void LoadProfileToFrontEnd(Profile p)
         {
+            isLoadingFrontend = true;
             switch_startonrun.IsChecked = p.autorun;
             tb_servaddr.Text = p.servAddr;
             tb_servport.Text = p.servPort;
@@ -82,6 +90,7 @@ namespace SnakeSocksClientGUI
             tb_debug_level.Text = p.debugLevel;
             tb_bindport.Text = p.bindPort;
             tb_bindaddr.Text = p.bindAddr;
+            isLoadingFrontend = false;
         }
         private void LoadProfileFromFrontEnd(Profile p)
         {
@@ -109,11 +118,81 @@ namespace SnakeSocksClientGUI
         }
         private void LoadConfigFile()
         {
+            XmlReader x = null;
+            try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                x = XmlReader.Create("skgui.xml", settings);
+                x.ReadStartElement("root");
+                tb_skcli_path.Text = x.ReadElementString("skcli");
+                while (true)
+                {
+                    try
+                    {
+                        x.ReadStartElement("profile");
+                    }
+                    catch (XmlException e)
+                    { //Read done.
+                        break;
+                    }
+                    Profile prof = new Profile();
+                    prof.Content = x.ReadElementString("name");
+                    prof.servAddr = x.ReadElementString("servaddr");
+                    prof.servPort = x.ReadElementString("servport");
+                    prof.bindAddr = x.ReadElementString("bindaddr");
+                    prof.bindPort = x.ReadElementString("bindport");
+                    prof.debugLevel = x.ReadElementString("debuglevel");
+                    prof.passphrase = x.ReadElementString("passphrase");
+                    prof.modulePath = x.ReadElementString("module");
+                    prof.autorun = x.ReadElementString("autoenable") == "True";
+                    listBox.Items.Add(prof);
+                    x.ReadEndElement();
+                }
+                x.ReadEndElement();
+                x.Close();
+            }
+            finally
+            {
+                if (x != null)
+                    x.Close();
+            }
 
         }
         private void SaveConfigFile()
         {
-
+            XmlWriter x = null;
+            try
+            {
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+                x = XmlWriter.Create("skgui.xml", settings);
+                x.WriteStartElement("root");
+                x.WriteElementString("skcli", tb_skcli_path.Text);
+                foreach (Profile prof in listBox.Items)
+                {
+                    if (String.IsNullOrEmpty(prof.Content.ToString()))
+                        continue;
+                    x.WriteStartElement("profile");
+                    x.WriteElementString("name", prof.Content.ToString());
+                    x.WriteElementString("servaddr", prof.servAddr);
+                    x.WriteElementString("servport", prof.servPort);
+                    x.WriteElementString("bindaddr", prof.bindAddr);
+                    x.WriteElementString("bindport", prof.bindPort);
+                    x.WriteElementString("debuglevel", prof.debugLevel);
+                    x.WriteElementString("passphrase", prof.passphrase);
+                    x.WriteElementString("module", prof.modulePath);
+                    x.WriteElementString("autoenable", prof.autorun.ToString());
+                    x.WriteEndElement();
+                }
+                x.WriteEndElement();
+                x.Flush();
+                x.Close();
+            }
+            finally
+            {
+                if (x != null)
+                    x.Close();
+            }
         }
         private class Profile : ListBoxItem
         {
@@ -141,7 +220,11 @@ namespace SnakeSocksClientGUI
             ClearFrontEnd();
             disable_profile_editor();
         }
-        
+        private void on_btn_save_conf(object sender, RoutedEventArgs e)
+        {
+            SaveConfigFile();
+        }
+
         private void on_listbox_select_changed(object sender, SelectionChangedEventArgs e)
         {
             if (e.AddedItems.Count < 1)
@@ -151,51 +234,106 @@ namespace SnakeSocksClientGUI
             Profile prof = item as Profile;
             LoadProfileToFrontEnd(prof);
         }
-        
+
         private void on_elem_input(object sender, RoutedEventArgs e)
-        {
+        { //Refresh memory
+            if (isLoadingFrontend) //If this event is sent by LoadProfileToFrontend.
+                return;
             Profile prof = listBox.SelectedItem as Profile;
-            if(prof == null)
+            if (prof == null)
             {
                 MessageBox.Show("Error: Selected Item is null.");
                 throw new NullReferenceException("null selected item");
             }
             LoadProfileFromFrontEnd(prof);
         }
-
-        private bool validate(Profile prof, string exePath)
-        {
-            //TODO
-            return true;
-        }
-        private void worker(Profile prof, string exePath)
-        {
-            Dispatcher.Invoke(delegate
+        private void on_autoenable_checked(object sender, RoutedEventArgs e)
+        { //Uncheck all other profile
+            if (isLoadingFrontend) //If this event is sent by LoadProfileToFrontend.
+                return;
+            foreach (Profile prof in listBox.Items)
             {
-                string exeArg = "-s " + prof.servAddr + " -p " + prof.servPort + " -k " + prof.passphrase + " -L " + prof.bindAddr + " -P " + prof.bindPort + " -D " + prof.debugLevel + " -m " + prof.modulePath;
+                if (prof.autorun)
+                    prof.autorun = false;
+            }
+            on_elem_input(sender, e);
+        }
 
-                if (!validate(prof, exePath))
-                    MessageBox.Show("Error: failed to validate your configuration. Check if you've filled all textBoxes and executable do exist.");
-                var proc = new Process
+        private string validate(Profile prof, string exePath)
+        {
+            int _;
+
+            if (prof.servAddr == "")
+                return "server address is required.";
+            if (prof.bindAddr == "")
+                return "local bind address is required.";
+            if (prof.passphrase == "")
+                return "passphrase is required.";
+
+            if (prof.modulePath.Contains(' '))
+                return "Space is not allowed in modulePath.";
+            if (prof.passphrase.Contains(' '))
+                return "Space is not allowed in passphrase.";
+
+            if (!System.IO.File.Exists(exePath))
+                return "skcli.exe not found.";
+            if (!System.IO.File.Exists(prof.modulePath))
+                return "module not found.";
+            if (!int.TryParse(prof.debugLevel, out _))
+                return "Invalid debug level";
+            if (!int.TryParse(prof.servPort, out _))
+                return "Invalid server port";
+            if (!int.TryParse(prof.bindPort, out _))
+                return "Invalid local bind port";
+
+            return "";
+        }
+        private string _exePath; //temp var.
+        private void worker(object _prof)
+        {
+            Profile prof = _prof as Profile;
+            string exeArg = "-s " + prof.servAddr + " -p " + prof.servPort + " -k " + prof.passphrase + " -L " + prof.bindAddr + " -P " + prof.bindPort + " -D " + prof.debugLevel + " -m " + prof.modulePath;
+            skcli_nogui = new Process
+            {
+                StartInfo = new ProcessStartInfo
                 {
-                    StartInfo = new ProcessStartInfo
-                    {
-                        FileName = exePath,
-                        Arguments = exeArg,
-                        UseShellExecute = false,
-                        RedirectStandardOutput = true,
-                        CreateNoWindow = true
-                    }
-                };
-                proc.Start();
-                while (!proc.StandardOutput.EndOfStream)
-                {
-                    string line = proc.StandardOutput.ReadLine();
-                    display.Text = display.Text + line + "\n";
+                    FileName = _exePath,
+                    Arguments = exeArg,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
                 }
-            });
+            };
+            display.Dispatcher.Invoke(() => 
+                display.Content += "\n---Launched---\n" + _exePath + " " + exeArg + "\n"
+            );
+
+            skcli_nogui.OutputDataReceived += delegate (object sender, DataReceivedEventArgs e)
+            {
+                try
+                {
+                    display.Dispatcher.Invoke(() => display.Content += e.Data + "\n", System.Windows.Threading.DispatcherPriority.Background);
+                }
+                catch (System.Threading.Tasks.TaskCanceledException _) { }
+            };
+            skcli_nogui.ErrorDataReceived += delegate (object sender, DataReceivedEventArgs e)
+            {
+                try
+                {
+                    display.Dispatcher.Invoke(() => display.Content += e.Data + "\n", System.Windows.Threading.DispatcherPriority.Background);
+                }
+                catch (System.Threading.Tasks.TaskCanceledException _) { }
+                //display.Text += e.Data + "\n";
+            };
+            //TODO: redirect stdout/err
+            skcli_nogui.Start();
+            skcli_nogui.BeginOutputReadLine();
+            skcli_nogui.BeginErrorReadLine();
+            //skcli_nogui.WaitForExit();
         }
         private Thread workerThread;
+        private Process skcli_nogui;
         private void on_enable(object sender, RoutedEventArgs e)
         {
             Profile prof = listBox.SelectedItem as Profile;
@@ -209,8 +347,15 @@ namespace SnakeSocksClientGUI
                 MessageBox.Show("Snakesocks is already running.");
                 return;
             }
-            workerThread = new Thread(() => worker(prof, tb_skcli_path.Text));
-            workerThread.Start();
+            string err = validate(prof, tb_skcli_path.Text);
+            if (err != "")
+            {
+                MessageBox.Show("Error: failed to validate your configuration: " + err);
+                return;
+            }
+            _exePath = tb_skcli_path.Text;
+            workerThread = new Thread(new ParameterizedThreadStart(worker));
+            workerThread.Start(prof);
         }
 
         private void on_disable(object sender, RoutedEventArgs e)
@@ -220,8 +365,23 @@ namespace SnakeSocksClientGUI
                 MessageBox.Show("Snakesocks is not running.");
                 return;
             }
-            workerThread.Abort();
+            if (skcli_nogui != null)
+            {
+                skcli_nogui.Kill();
+                skcli_nogui = null;
+            }
             workerThread = null;
+            display.Content += "\n---Stopped---\n";
+        }
+
+        private void on_display_copy(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(display.Content.ToString());
+        }
+
+        private void on_display_clear(object sender, RoutedEventArgs e)
+        {
+            display.Content = "";
         }
     }
 }
---
+++ src/client-for-gui.cc
@@ -0,0 +1,138 @@
+#include "socks5.hpp"
+#include "debug.hpp"
+#include "config.hpp"
+#include "syserr.hpp"
+
+#include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+int _runtime_debugLevel;
+
+using namespace std;
+
+int main_proc(config &conf)
+{
+    _runtime_debugLevel = conf.debugLevel;
+    cout << "Setting debug level = " << _runtime_debugLevel << endl;
+    client_module mod(conf.serverIp, conf.serverPort, conf.passphrase, conf.modulePath);
+    s5server sss(conf.bindIp, conf.bindPort, tunnel(std::move(mod)));
+    sss.run();
+    return 0;
+}
+
+void display_usage()
+{
+#define n "\n"
+    printf(
+            "SnakeSocks Client 1.1.4 Special edition for WIN32GUI | no stderr and no exception" n n
+            "Usage: snakesockscli [OPTION...]" n
+            "A extensible proxy which supports module-defined protocol behavior." n
+            "You can install and run module easily from configuration file," n
+            "to determine how packets are encrypted, transmitted, etc." n n
+            "Options:" n
+            "-s <server address>\tServer ip or domain" n
+            "-p <server port>\tServer listening port" n
+            "-k <passphrase>\tPassphrase" n
+            "-L <listen address>\tLocal address that socks5 server listens on" n
+            "-P <listen port>\tLocal port that socks5 server listens on" n
+            "-D <debug level>\tDebug level to set" n
+            "-m <path to module>\tModule path" n
+            n
+            "-h\tShow this message" n
+            "NEVER launch this program directly!" n n
+            "Published on GNU license V2." n
+    );
+#undef n
+}
+
+int ____main(int arglen, char **argv)
+{
+    int dflag = 0;
+    char *confPath = NULL;
+    char *daemonLogFilePath = NULL;
+    char *serverAddr = NULL, *serverPort = NULL, *passP = NULL, *listenAddr = NULL, *listenPort = NULL;
+    char *debugLev = NULL, *modPath = NULL;
+    int index;
+    int c;
+
+    opterr = 0;
+
+    while ((c = getopt (arglen, argv, "c:dhl:s:p:k:L:P:D:m:")) != -1)
+        switch (c)
+        {
+            case 'd':
+                dflag = 1;
+                break;
+            case 'c':
+                confPath = optarg;
+                break;
+            case 'h':
+                display_usage();
+                exit(0);
+                break;
+            case 'l':
+                daemonLogFilePath = optarg;
+                break;
+            case 's':
+                serverAddr = optarg;
+                break;
+            case 'p':
+                serverPort = optarg;
+                break;
+            case 'k':
+                passP = optarg;
+                break;
+            case 'L':
+                listenAddr = optarg;
+                break;
+            case 'P':
+                listenPort = optarg;
+                break;
+            case 'D':
+                debugLev = optarg;
+                break;
+            case 'm':
+                modPath = optarg;
+                break;
+            case '?':
+                if (optopt == 'c')
+                    printf("Option -%c requires an argument.\n", optopt);
+                else if (isprint (optopt))
+                    printf("Unknown option `-%c'.\n", optopt);
+                else
+                    printf("Unknown option character `\\x%x'.\n", optopt);
+                display_usage();
+                return 1;
+            default:
+                abort();
+        }
+
+    config conf;
+    if(daemonLogFilePath) die("Daemon file path not supported.");
+    if(!serverAddr) die("servAddr not set."); conf.serverIp = serverAddr;
+    if(!serverPort) die("servPort not set."); conf.serverPort = atoi(serverPort);
+    if(!passP) die("passphrase not set."); conf.passphrase = passP;
+    if(!listenAddr) die("bindAddr not set."); conf.bindIp = listenAddr;
+    if(!listenPort) die("bindPort not set."); conf.bindPort = atoi(listenPort);
+    if(!debugLev) die("debugLevel not set."); conf.debugLevel = atoi(debugLev);
+    if(!modPath) die("modulePath not set."); conf.modulePath = modPath;
+	
+    if(dflag) die("Daemon mode is not currently supported on WinNT. You can launch it as service by yourself.");
+
+    return main_proc(conf);
+}
+
+int main(int l, char **p)
+{
+	try{
+		____main(l, p);
+	}
+	catch(std::exception &e){
+		std::cout << "Exception uncaught: " << e.what() << "\n";
+	}
+	catch(...){
+		std::cout << "Unknown exception...... Some exception doesn't inherit std::exception? qaq...\n";
+	}
+}
